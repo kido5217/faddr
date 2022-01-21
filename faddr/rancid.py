6d974d47d7e4889befddab354c59a1ea4de71293
Fedor Suchkov <f.suchkov@gmail.com>
2022-01-21 16:17:28 +0300

    Rewrite RancidDir and RancidRoot again

--- faddr/rancid.py
+++ faddr/rancid.py
@@ -11,125 +11,145 @@ from faddr.exceptions import (
 )
 
 
-class RancidDir:
-    """Analyze dir and find rancid repos/backup files.
+class RancidGroup:
+    """Rancid group dir parser.
 
-    path can be one of three levels of Rancid base dir:
-
-    rancid_dir/                 <- "root"
-    ├── group1                  <- "group"
-    │   ├── configs             <- "repo"
+    rancid_dir
+    ├── group1                  <- This Class, self.type="group"
+    │   ├── configs             <- This Class, self.type="repo"
     │   │   └── 7206-ios15-1
     │   ├── router.db
-
     """
 
-    def __init__(self, path, level) -> None:
-        if level in ("root", "group", "repo"):
-            self.level = level
+    def __init__(self, path, name=None):
+        self.path = pathlib.Path(path)
+        if not self.path.exists() or not self.path.is_dir():
+            raise RuntimeError(f"Wrong path: {path}")
+
+        if name:
+            self.name = name
         else:
-            raise FaddrRancidRepoUnsupportedLevel
+            self.name = self.path.name
 
-        path = pathlib.Path(path)
-        if path.exists() and path.is_dir():
-            self.path = path
+        if pathlib.Path(self.path, "router.db").exists():
+            self.type = "group"
+            self.repo = pathlib.Path(self.path, "configs")
         else:
-            raise FaddrRancidRepoPathError(path)
+            self.type = "repo"
+            self.repo = self.path
 
-        self.configs = []
+        if self.type == "group":
+            self.configs = self.get_configs_from_router_db()
+        else:
+            self.configs = self.get_configs_from_dir()
 
-        if self.level in ("repo", "group"):
-            self.groups = [self.path.name]
-            self.configs.extend(self.get_configs(self.path))
+    def __str__(self):
+        return f"RANCID {self.type} '{self.name}' in '{self.path}' contains {len(self.configs)} configs"
 
-        elif self.level == "root":
-            pass
+    def get_configs_from_router_db(self):
+        """Load config files list and their metadata from 'router.db' file."""
 
-    def get_configs(self, path):
-        """Make list of dicts with config file data."""
         configs = []
-        if self.level == "repo":
-            path = pathlib.Path(path)
-            # With repo level, group name equals to dir name and all devices are enabled
-            group = path.name
-            for child in path.iterdir():
-                if child.is_file():
-                    try:
-                        config = {}
-                        config["content_type"] = self.get_content_type(child)
-                        config["group"] = group
-                        config["path"] = child
-                        config["enabled"] = True
-                        configs.append(config)
-                    except FaddrRancidRepoConfigFileFormatError:
-                        logger.info(f"File {child} isn't valid rancid config backup")
-        else:
-            group = pathlib.Path(path).name
-            router_db_path = pathlib.Path(path, "router.db")
-            if router_db_path.exists():
-                configs = self.parse_router_db(router_db_path)
 
+        with pathlib.Path(self.path, "router.db").open(
+            encoding="ascii", errors="ignore"
+        ) as router_db_file:
+            router_db = router_db_file.readlines()
+
+        for router_string in router_db:
+            if router_string.startswith("#"):
+                continue
+
+            if ";" in router_string:
+                sep = ";"
             else:
-                raise FaddrRancidRepoRouterDBAbsent
+                sep = ":"
+
+            # router.db is actually a csv file, but python's csv's builtin
+            # dialect parser didn't work for me, so manual parsing for now
+            router_data = router_string.strip().strip(";:").split(sep)
+            if len(router_data) == 3 or len(router_data) == 4:
+                config = {}
+                enabled_map = {"up": True, "down": False}
+                config["enabled"] = enabled_map.get(router_data[2], False)
+                config["path"] = pathlib.Path(self.repo, router_data[0])
+                config["content_type"] = router_data[1]
+                config["name"] = router_data[0]
+                if len(router_data) == 3:
+                    config["hostname"] = router_data[0]
+                else:
+                    config["hostname"] = router_data[3]
+                config["router_db_raw_string"] = router_string
+                configs.append(config)
 
         return configs
 
-    @staticmethod
-    def get_content_type(path, encoding="ascii", errors="ignore"):
-        """Get rancid content type from file."""
-        with pathlib.Path(path).open(encoding=encoding, errors=errors) as config_file:
-            content_type_string = config_file.readline().strip().split(" ")
-        if len(content_type_string) == 2:
-            content_type = content_type_string[1]
-        else:
-            raise FaddrRancidRepoConfigFileFormatError(path)
-        return content_type
+    def get_configs_from_dir(self):
+        """Load config files list and their metadata from dir with files itself."""
 
-    @staticmethod
-    def parse_router_db(path, encoding="ascii", errors="ignore"):
-        """Parse router.db file into list of dicts."""
         configs = []
-        is_enabled_map = {
-            "up": True,
-            "down": False,
-        }
-        router_db_path = pathlib.Path(path)
-        group = router_db_path.parent.name
 
-        with router_db_path.open(encoding=encoding, errors=errors) as router_db:
-            for device_string in router_db.readlines():
-                # Ignore conneted out devices
-                if device_string.startswith("#"):
-                    logger.info(f"Ingoring device {device_string} in {router_db_path}")
-                    continue
+        for config_path in self.repo.iterdir():
+            if config_path.stat().st_size == 0:
+                continue
 
-                if ";" in device_string:
-                    sep = ";"
-                else:
-                    sep = ":"
-
-                device = device_string.split(sep)
-                if len(device) == 3:
-                    config = {}
-                    config["path"] = router_db_path.parent.joinpath(
-                        "configs", device[0]
-                    )
-                    config["content_type"] = device[1]
-                    config["group"] = group
-                    config["enabled"] = is_enabled_map.get(device[2], False)
-                    configs.append(config)
-                elif len(device) == 4:
-                    config = {}
-                    config["path"] = router_db_path.parent.joinpath(
-                        "configs", device[0]
-                    )
-                    config["content_type"] = device[2]
-                    config["group"] = group
-                    config["enabled"] = is_enabled_map.get(device[3], False)
-                    configs.append(config)
-                else:
-                    logger.info(
-                        f"Malformed device string {device} in {router_db_path}, ignoring"
-                    )
+            content_type = self.get_content_type(config_path)
+            if content_type:
+                config = {}
+                config["enabled"] = True
+                config["path"] = config_path
+                config["content_type"] = content_type
+                config["name"] = config_path.name
+                config["hostname"] = config_path.name
+                configs.append(config)
 
         return configs
+
+    @staticmethod
+    def get_content_type(config_path):
+        """Get rancid profile name from file header."""
+
+        with pathlib.Path(config_path).open(
+            encoding="ascii", errors="ignore"
+        ) as config:
+            content_type_line = config.readline().strip()
+
+        if (
+            "RANCID-CONTENT-TYPE" in content_type_line
+            and len(content_type_line.split(" ")) > 1
+        ):
+            return content_type_line.split(" ")[1]
+
+        return None
+
+
+class RancidDir:
+    """Rancid root dir parser.
+
+    rancid_dir/                 <- This Class
+    ├── group1
+    │   ├── configs
+    │   │   └── 7206-ios15-1
+    │   ├── router.db
+    """
+
+    def __init__(self, path):
+        self.path = pathlib.Path(path)
+        if not self.path.exists() or not self.path.is_dir():
+            raise RuntimeError(f"Wrong path: {path}")
+
+        self.groups = []
+        for group_candidate in self.path.iterdir():
+            try:
+                if (
+                    group_candidate.is_dir()
+                    and pathlib.Path(group_candidate, "router.db").exists()
+                ):
+                    rancid_group = RancidGroup(group_candidate)
+                    if len(rancid_group.configs) > 0:
+                        self.groups.append(rancid_group)
+            except PermissionError:
+                print(f"Can't access {group_candidate}")
+
+    def __str__(self):
+        return f"RANCID directory '{self.path}' contains {len(self.groups)} groups"
