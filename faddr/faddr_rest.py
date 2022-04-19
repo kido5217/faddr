8db2cbbdd5593719c44b8ac6632c7c175ae0a955
Fedor Suchkov <f.suchkov@gmail.com>
2022-04-19 22:47:18 +0300

    WIP: network GET

--- faddr/faddr_rest.py
+++ faddr/faddr_rest.py
@@ -1,5 +1,25 @@
 """REST API entry point."""
 
+import uvicorn
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    """API root."""
+    return {"message": "Hello World"}
+
+
+@app.get("/networks/")
+async def get_networks(query):
+    """Find network"""
+    return {query: "network_data"}
+
 
 def main():
     """Start gunicorn server."""
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
